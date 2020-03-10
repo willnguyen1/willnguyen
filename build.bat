103b19384196e96435f96bb4c5e4b3527535f452
Linh Nguyen <linh.nguyen.it91@gmail.com>
2020-03-11 00:17:11 +0700

    blogs

---
+++ build.bat
@@ -0,0 +1,1 @@
+cd C:/Users/Will/Desktop/code/bio2/ && bundle exec jekyll build && xcopy /S /I /E C:\Users\Will\Desktop\code\bio2\_site C:\Users\Will\Desktop\code\z_willnguyen_github && TIMEOUT /T 10
